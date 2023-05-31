1c699a252655f405cf269109ff70ac391b132dd7
Niki Muhammad Nafis <nikinafis08@gmail.com>
2023-05-31 23:53:06 +0700

    add SignInFragment

---
+++ app/src/main/java/com/jejaka/jejaka_app/ui/auth_page/SignInFragment.kt
@@ -0,0 +1,160 @@
+package com.jejaka.jejaka_app.ui.auth_page
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Patterns
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import com.jejaka.jejaka_app.MainActivity
+import com.jejaka.jejaka_app.R
+import com.jejaka.jejaka_app.databinding.FragmentSignInBinding
+import com.jejaka.jejaka_app.ui.user.SignUpActivity
+
+class SignInFragment : Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.actionBar?.hide()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupAction()
+        emailFocusListener()
+        passwordFocusListener()
+
+        binding.btnLogin.setOnClickListener { submitForm() }
+    }
+
+    private fun submitForm()
+    {
+        binding.emailContainer.helperText = validEmail()
+        binding.passwordContainer.helperText = validPassword()
+
+        val validEmail = binding.emailContainer.helperText == null
+        val validPassword = binding.passwordContainer.helperText == null
+
+        if (validEmail && validPassword)
+            startActivity(
+                Intent(
+                    activity, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+        else
+            invalidForm()
+    }
+
+    private fun invalidForm()
+    {
+        var message = ""
+        if(binding.emailContainer.helperText != null)
+            message += "\n\nEmail: " + binding.emailContainer.helperText
+        if(binding.passwordContainer.helperText != null)
+            message += "\n\nPassword: " + binding.passwordContainer.helperText
+
+        AlertDialog.Builder(requireContext())
+            .setTitle("Invalid Form")
+            .setMessage(message)
+            .setPositiveButton("Okay"){ _,_ ->
+                // do nothing
+            }
+            .show()
+    }
+
+    private fun resetForm()
+    {
+        var message = "Email: " + binding.emailEditText.text
+        message += "\nPassword: " + binding.passwordEditText.text
+        AlertDialog.Builder(requireContext())
+            .setTitle("Form submitted")
+            .setMessage(message)
+            .setPositiveButton("Okay"){ _,_ ->
+                binding.emailEditText.text = null
+                binding.passwordEditText.text = null
+
+                binding.emailContainer.helperText = getString(R.string.required)
+                binding.passwordContainer.helperText = getString(R.string.required)
+            }
+            .show()
+    }
+
+    private fun emailFocusListener()
+    {
+        binding.emailEditText.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+            {
+                binding.emailContainer.helperText = validEmail()
+            }
+        }
+    }
+
+    private fun validEmail(): String?
+    {
+        val emailText = binding.emailEditText.text.toString()
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailText).matches())
+        {
+            return "Invalid Email Address"
+        }
+        return null
+    }
+
+    private fun passwordFocusListener()
+    {
+        binding.passwordEditText.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+            {
+                binding.passwordContainer.helperText = validPassword()
+            }
+        }
+    }
+
+    private fun validPassword(): String?
+    {
+        val passwordText = binding.passwordEditText.text.toString()
+        if(passwordText.length < 8)
+        {
+            return "Minimum 8 Characters"
+        }
+        if(!passwordText.matches(".*[A-Z].*".toRegex()))
+        {
+            return "Must Contain 1 Upper-case Character"
+        }
+        if(!passwordText.matches(".*[a-z].*".toRegex()))
+        {
+            return "Must Contain 1 Lower-case Character"
+        }
+        if(!passwordText.matches(".*[@#\$%^&+=].*".toRegex()))
+        {
+            return "Must Contain 1 Special Character (@#\$%^&+=)"
+        }
+
+        return null
+    }
+
+    private fun setupAction() {
+
+        binding.tvSignup.setOnClickListener{
+            startActivity(
+                Intent(
+                    activity, SignUpActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+        }
+    }
+
+    companion object {
+
+    }
+}
