236c082ee836f67319b02e2735555e224460d04b
nurvibrgtyn <brigityana.nurvi@gmail.com>
2023-06-16 22:06:06 +0700

    Add AboutActivity, App Icon, change language to Indonesian

---
+++ app/src/main/java/com/jejaka/jejaka_app/ui/about/AboutActivity.kt
@@ -0,0 +1,12 @@
+package com.jejaka.jejaka_app.ui.about
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jejaka.jejaka_app.R
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
